7370520f75c72611eda2f097b1e20a7ae39ad66d
Tati <fanny201@mail.ru>
2023-06-01 23:43:02 +0300

    Done

---
+++ Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine($"{a} в степени {b} равно: " + step);
---
+++ Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива:\t");
+int elementsCount = int.Parse(Console.ReadLine());
+int[] myArray = new int[elementsCount];
+for(int i = 0; i < elementsCount; i++)
+{
+    Console.WriteLine("Введите {0}-й элемент", i + 1);
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+          
+Console.WriteLine("Вывод массива: ");
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i]);
+}
+Console.ReadLine();    
